b28f1bd85295ae4a2ec90dc97a5cd977a7d7ce13
Erin Stansbury <ekstansbury@gmail.com>
2020-04-04 21:14:06 +0000

    Automatically backed up by Learn

--- spec/cash_register_spec.rb
+++ spec/cash_register_spec.rb
@@ -1,33 +1,73 @@
-class CashRegister
-  attr_accessor :discount, :last_transaction, :total
 
-  def initialize(discount = 0)
-    @items = []
-    @discount = discount
-    @total = 0
   end
 
-  def add_item(name, cost, number = 1)
-    self.total += (cost*number)
-    @last_transaction = cost
-    number.times{@items.push(name)}
-  end
+  describe '#add_item' do
+    it 'accepts a title and a price and increases the total' do
+      expect{cash_register.add_item("eggs", 0.98)}.to change{cash_register.total}.by(0.98)
+    end
 
-  def apply_discount
-    if discount != 0
-      @total -= @total*(@discount/100.00)
-      return "After the discount, the total comes to $#{(self.total).to_i}."   # to_i invoked here only to pass tests.
-    else
-      return "There is no discount to apply."
+    it 'also accepts an optional quantity' do
+      expect{cash_register.add_item("book", 5.00, 3)}.to change{cash_register.total}.by(15.00)
+    end
+
+    it "doesn't forget about the previous total" do
+      cash_register.add_item("Lucky Charms", 4.5)
+      expect(cash_register.total).to eq(4.5)
+      cash_register.add_item("Ritz Crackers", 5.0)
+      expect(cash_register.total).to eq(9.5)
+      cash_register.add_item("Justin's Peanut Butter Cups", 2.50, 2)
+      expect(cash_register.total).to eq(14.5)
     end
   end
 
-  def items
-    return @items
+  describe '#apply_discount' do
+    context 'the cash register was initialized with an employee discount' do
+      it 'applies the discount to the total price' do
+        cash_register_with_discount.add_item("macbook air", 1000)
+        cash_register_with_discount.apply_discount
+        expect(cash_register_with_discount.total).to eq(800)
+      end
+
+      it 'returns success message with updated total' do
+        cash_register_with_discount.add_item("macbook air", 1000)
+        expect(cash_register_with_discount.apply_discount).to eq("After the discount, the total comes to $800.")
+      end
+
+      it 'reduces the total' do
+        cash_register.total = 0
+        cash_register_with_discount.add_item("macbook air", 1000)
+        expect{cash_register_with_discount.apply_discount}.to change{cash_register_with_discount.total}.by(-200)
+      end
+    end
+
+    context 'the cash register was not initialized with an employee discount' do
+      it 'returns a string error message that there is no discount to apply' do
+        expect(cash_register.apply_discount).to eq("There is no discount to apply.")
+      end
+    end
   end
 
-  def void_last_transaction
-    self.total -= @last_transaction
+  def '#items' do
+    it 'returns an array containing all items that have been added' do
+      new_register = CashRegister.new
+      new_register.add_item("eggs", 1.99)
+      new_register.add_item("tomato", 1.76, 3)
+      expect(new_register.items).to eq(["eggs", "tomato", "tomato", "tomato"])
+    end
   end
 
+  def '#void_last_transaction' do
+    it 'subtracts the last item from the total' do
+      cash_register.add_item("apple", 0.99)
+      cash_register.add_item("tomato", 1.76)
+      cash_register.void_last_transaction
+      expect(cash_register.total).to eq(0.99)
+    end
+
+    it 'returns the total to 0.0 if all items have been removed' do
+      cash_register.add_item("tomato", 1.76, 2)
+      expect{cash_register.void_last_transaction}.to change{cash_register.total}.from(3.52).to(0.0)
+    end
+  end
 end
+
